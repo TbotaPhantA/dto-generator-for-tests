1a816a60ea3b57454ff146126f0b4fba324cc670
TbotaPhantA <botakiller@mail.ru>
2022-01-16 21:39:16 +0300

    example dto was added with example enum

---
+++ src/exampleDto/example.dto.ts
@@ -0,0 +1,29 @@
+import {ExampleEnum} from "./example.enum";
+
+class ExampleDto {
+    requiredNumberField: number;
+    optionalNumberField?: number;
+
+    requiredStringField: string;
+    optionalStringField?: string;
+
+    requiredEnumField: ExampleEnum;
+    optionalEnumField?: ExampleEnum;
+
+    requiredDateField: Date;
+    optionalDateField?: Date;
+
+    // TODO: fields for required and optional *DtoFields or [*DtoFields]
+
+    constructor(
+        requiredNumberField: number,
+        requiredStringField: string,
+        requiredEnumField: ExampleEnum,
+        requiredDateField: Date,
+    ) {
+        this.requiredNumberField = requiredNumberField;
+        this.requiredStringField = requiredStringField;
+        this.requiredEnumField = requiredEnumField;
+        this.requiredDateField = requiredDateField;
+    }
+}
