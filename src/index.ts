986ec54041d0ff81e7c6c9a36025e5eee7eb5416
TbotaPhantA <botakiller@mail.ru>
2022-01-17 23:18:32 +0300

    unfinished attempt to make not status dto Generator

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,38 @@
-export const Greeter = (name: string) => `Hello ${name}`
+import { ExampleDto } from './exampleDto/example.dto';
+import { ExampleEnum } from './exampleDto/example.enum';
+
+class ExampleDtoGenerator extends ExampleDto {
+    withRequiredNumberField(requiredNumberField: number) {
+       this.requiredNumberField = requiredNumberField
+       return this
+    }
+
+    withRequiredStringField(requiredStringField: string) {
+        this.requiredStringField = requiredStringField
+       return this
+    }
+
+    withRequiredEnumField(requiredEnumField: ExampleEnum) {
+        this.requiredEnumField = requiredEnumField;
+       return this
+    }
+
+    withRequiredDateField(requiredDateField: Date) {
+        this.requiredDateField = requiredDateField
+       return this
+    }
+
+    default(): ExampleDtoGenerator {
+        return this
+            .withRequiredNumberField(1)
+            .withRequiredStringField("string")
+            .withRequiredEnumField(ExampleEnum.exampleEnumOption1)
+            .withRequiredDateField(new Date())
+    }
+}
+
+const main = () => {
+    // const generator = new ExampleDtoGenerator();
+}
+
+main();
