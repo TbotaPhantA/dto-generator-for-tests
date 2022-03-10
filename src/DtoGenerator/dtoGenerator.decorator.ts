a7413123c6380dc988ac199afd7cd8c4bab678a1
TbotaPhantA <botakiller@mail.ru>
2022-03-10 19:24:12 +0000

    decorator which pulls property names was initialized

--- src/DtoGenerator/dtoGenerator.decorator.ts
+++ src/DtoGenerator/dtoGenerator.decorator.ts
@@ -7,22 +7,10 @@
 const DtoGenerator =
   <T extends { new(...args: any[]): {} }>(constructor: T) => {
     return class extends constructor {
-      // TODO: generator
-      public withFirstProperty: (firstKey) => this;
+      public constructor(...any: any[]) {
+        super();
+        const propertyNames = Object.keys(this)
 
-      static default() {
-        return new this().makeWithMethods();
-      }
-
-      public makeWithMethods() {
-        const firstKey = Object.keys(this)[0]
-
-        this.withFirstProperty = function (firstKey) {
-          this[firstKey] = firstKey;
-          return this
-        }
-
-        return this
       }
     };
   };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { ExampleDto } from './exampleDto/example.dto';
 import { DtoGenerator } from './DtoGenerator/dtoGenerator.decorator';
 
-// eslint-disable-next-line new-cap
 const ExampleDtoGenerator = DtoGenerator(ExampleDto);
 
 const main = () => {
-  console.log(Object.keys(ExampleDtoGenerator.default()));
+  console.log(new ExampleDtoGenerator());
 };
 
 main();
