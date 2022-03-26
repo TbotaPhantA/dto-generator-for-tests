06860059b7b1da25f626797c59862b84a0450953
TbotaPhantA <botakiller@mail.ru>
2022-03-26 19:58:35 +0000

    structure of the library was changed and tsconfig file was changed

--- src/exampleDto/example.enum.ts
+++
@@ -1,9 +0,0 @@
-export enum ExampleEnum {
-    exampleEnumOption1 = 'exampleEnumOption1',
-    exampleEnumOption2 = 'exampleEnumOption2',
-    exampleEnumOption3 = 'exampleEnumOption3',
-    exampleEnumOption4 = 'exampleEnumOption4',
-    exampleEnumOption5 = 'exampleEnumOption5',
-    exampleEnumOption6 = 'exampleEnumOption6',
-    exampleEnumOption7 = 'exampleEnumOption7',
-}
