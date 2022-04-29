f4da68308305672440e24a176991907c915a4dba
TbotaPhantA <botakiller@mail.ru>
2022-04-29 15:14:43 +0000

    playground example was prepared

--- lib/__fixtures__/playground.ts
+++ lib/__fixtures__/playground.ts
@@ -1,14 +1,40 @@
-import InjectionGenerator from '../generators/injectionGenerator';
-
 class Dto {
   field1: number = 1;
   field2: string = '';
 }
 
-class DtoGenerator extends InjectionGenerator<Dto> {
-  public static default() {
-    return new InjectionGenerator(new Dto())
+class DtoGenerator {
+
+  // ----- AUTOMATICALLY -----
+
+  public readonly result: Dto;
+
+  constructor(
+    private readonly _target: Dto,
+  ) {
+    this.result = _target
+  }
+
+  withField1(field1: number) {
+    this._target.field1 = field1
+    return this
+  }
+
+  withField2(field2: string) {
+    this._target.field2 = field2
+    return this
+  }
+
+  // ----- MANUALLY -----
+
+  public static default(): DtoGenerator {
+    return new DtoGenerator(new Dto())
   }
 }
 
-const something = DtoGenerator.default().result
+const something = DtoGenerator.default()
+  .withField1(1)
+  .withField2('something')
+  .result
+
+
